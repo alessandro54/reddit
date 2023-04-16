a1c9175ca4041b55f8975e89e3e42021063393a7
Alessandro Chumpitaz <par_214@hotmail.com>
2023-04-16 01:05:44 -0500

    :bug: fixed assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:reddit/navigator.dart' as nav;
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-Future main() async {
-  await dotenv.load(fileName: ".env");
+void main() {
   runApp(const MyApp());
 }
 
